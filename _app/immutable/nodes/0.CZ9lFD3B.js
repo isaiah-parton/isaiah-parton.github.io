0419f6462f491ccb45dc7024b626148ac43058e8
Isaiah Parton <isaiahtheparton@gmail.com>
2025-06-21 23:09:37 -0600

    .

---
+++ _app/immutable/nodes/0.CZ9lFD3B.js
@@ -0,0 +1,1 @@
+import{c as n,b as a}from"../chunks/B4MyQsV6.js";import{a0 as p}from"../chunks/C1gHS2es.js";import{s}from"../chunks/CE28BuxK.js";const i=!0,f=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=p(e);s(t,()=>r.children),a(o,e)}export{u as component,f as universal};
